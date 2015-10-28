1fd9feb8433d1fa2e439b0406e45f1814e8253a5
Laurens Allart <laurensallart@MacBook-Pro-van-Laurens-Allart.local>
2015-10-28 12:38:39 +0100

    create thermostat
    creating a thermostat is now done with a UDP message to the server. The
    thermostat can than be linked to the user through the unique code.

--- public/modules/thermostats/services/thermostats.client.service.js
+++ public/modules/thermostats/services/thermostats.client.service.js
@@ -3,6 +3,7 @@
 //Thermostats service used to communicate Thermostats REST endpoints
 angular.module('thermostats').factory('Thermostats', ['$resource',
 	function($resource) {
+		
 		return $resource('thermostats/:thermostatId', { thermostatId: '@_id'
 		}, {
 			update: {
